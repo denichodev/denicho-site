ccf0c748da54a56b879a79d45ed09d470646984a
Deni Cho <denichodev@gmail.com>
2025-06-28 22:33:23 +0200

    critical css

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,7 +3,14 @@ import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 
 export default function (eleventyConfig) {
   eleventyConfig.addWatchTarget("css/**/*.css");
-  eleventyConfig.addPassthroughCopy("css/**/*.css");
+  // eleventyConfig.addPassthroughCopy("css/**/*.css");/
+
+  eleventyConfig.addBundle("css", {
+    toFileDirectory: "dist",
+    // Add all <style> content to `css` bundle (use <style eleventy:ignore> to opt-out)
+    // Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
+    bundleHtmlContentFromSelector: "style",
+  });
 
   eleventyConfig.addPassthroughCopy("fonts/**/*.{woff,woff2,ttf,otf,eot,svg}");
 
