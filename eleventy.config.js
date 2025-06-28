90269a8b906b11471c7aa7e55ccc2c38d8051707
Deni Cho <denichodev@gmail.com>
2025-06-28 18:35:45 +0200

    change directory and structures

---
+++ eleventy.config.js
@@ -0,0 +1,14 @@
+import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
+}
+
+export const config = {
+  dir: {
+    input: "content",
+    includes: "../_includes", // default: "_includes" (`input` relative)
+    data: "../_data", // default: "_data" (`input` relative)
+    output: "_site",
+  },
+};
